17556f56548220f9d03933fee40de19130e4922a
Rui de Alencar Frota <ruidealencar@gmail.com>
2022-06-26 21:19:19 -0300

    Lesson component create

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,19 @@
+import { CheckCircle } from "phosphor-react";
+
 export function Lesson() {
-  return <div>Lesson</div>;
+  return (
+    <a href="#">
+      <span className="text-gray-300"></span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <CheckCircle size={20} />
+          </span>
+          <span className="text-xs rounded py[0.125rem] px-2"></span>
+        </header>
+        <strong className="text-gray-200 mt-5 block"></strong>
+      </div>
+    </a>
+  );
 }
